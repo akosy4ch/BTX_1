a11a71d19b87160dea66e962e676dd220363aa78
akosy4ch <121079410+akosy4ch@users.noreply.github.com>
2024-01-04 22:58:19 +0600

    Update HelloWorld3.sol

--- contracts/HelloWorld3.sol
+++ contracts/HelloWorld3.sol
@@ -1,25 +1,45 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.12 <0.9.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Aitu_Alibek_your_Kamiluly is ERC20 {
-    using Strings for uint256;
+    event TransferInfo(uint256 timestamp, address sender, address receiver, uint256 amount);
 
-    constructor() ERC20("<Aitu_Alibek_your_Kamiluly>", "UNI") {
-        _mint(msg.sender, 2000 * (10 ** decimals()));
+    constructor() ERC20("AITU Alibek your Kamiluly", "AITU") {
+        _mint(msg.sender, 2000 * 10**18); // Initial supply
     }
 
-    function getLatestTransactionTimestamp() public view returns (string memory) {
-        return uint256(block.timestamp).toString();
-    }
+    
+    function getLatestTransactionTimestamp() external view returns (string memory) {
+    return Strings.toString(block.timestamp);
+  }
 
-    function getTransactionSender() public view returns (address) {
+    
+    function getTransactionSenderAddress() external view returns (address) {
         return msg.sender;
     }
 
-    function getTransactionReceiver() public view returns (address) {
-        return address(0);
+    
+    function getTransactionReceiverAddress() external view returns (address) {
+        return address(this);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        bool success = super.transfer(recipient, amount);
+        if (success) {
+            emit TransferInfo(block.timestamp, msg.sender, recipient, amount);
+        }
+        return success;
+    }
+
+   
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        bool success = super.transferFrom(sender, recipient, amount);
+        if (success) {
+            emit TransferInfo(block.timestamp, sender, recipient, amount);
+        }
+        return success;
     }
 }
